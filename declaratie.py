5806f29f35a9e2ccb1184d99ca62c6febab774cc
adrianraduta <64138061+adrianraduta@users.noreply.github.com>
2020-04-22 17:46:21 +0300

    Create declaratie.py

---
+++ declaratie.py
@@ -0,0 +1,13 @@
+nume_angajat:str = input("Care este numele tau? ")
+prenume_angajat:str = input("Care este prenumele tau? ")
+adresa:str = input("Care este adresa ta? ")
+motivul:str = input("Care este motivul? ")
+data:int = input("In ce data suntem astazi? ")
+eroare:str = "Trebuie introduse date "
+if nume_angajat and prenume_angajat and adresa and motivul and data:
+    print("Declaratia este pentru" + " " + nume_angajat + " " + prenume_angajat)
+    print("Adresa este " + " " + adresa)
+    print("Motivul este " + " " + motivul)
+    print("Data declaratiei " + " " + data)
+else:
+    print(eroare)
